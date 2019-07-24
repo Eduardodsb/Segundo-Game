6cfbe5eefa1e92cedf15335a508db84dbc3e4d90
Eduardo Barbosa <33110840+Eduardodsb@users.noreply.github.com>
2019-07-24 18:42:00 -0300

    #Camera, mapa,  +CameraMovement
    -Foi alterada o modo da câmera e o mapa

---
+++ segundo-game/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour{
+
+    public Transform transform;
+   public GameObject player;
+   Vector3 positionCam;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        positionCam.x = player.GetComponent<Transform>().position.x;
+        positionCam.y = transform.position.y;
+        positionCam.z = -10;
+        transform.position = positionCam;
+    }
+}

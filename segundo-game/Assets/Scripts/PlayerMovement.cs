a5da9a71cbdf834621d62cfb1d0ccb5c628b0115
Eduardo Barbosa <33110840+Eduardodsb@users.noreply.github.com>
2019-07-23 21:22:13 -0300

    +Animations

--- segundo-game/Assets/Scripts/PlayerMovement.cs
+++ segundo-game/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour{
 
     public CharacterController2D controller;
-    float horizontalMove = 0;
     public float runSpeed = 40f;
+    public Animator animator;
+    public Rigidbody2D rb;
+
+    bool  collision = false;
+    float horizontalMove = 0;
+  
     bool jump = false;
     bool crouch = false;
 
@@ -18,9 +23,11 @@ public class PlayerMovement : MonoBehaviour{
     // Update is called once per frame
     void Update(){
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
         if (Input.GetButtonDown("Jump")){
             jump = true;
+            //animator.SetBool("IsJumping", true);
         }
 
         if (Input.GetButtonDown("Crouch")){
@@ -29,10 +36,43 @@ public class PlayerMovement : MonoBehaviour{
             crouch = false;
         }
     }
+    
+    public void OnLanding(){
+       
+        //animator.SetBool("IsJumping", false);
+    }
+
     void FixedUpdate(){
+        if (rb.velocity.y > 0.0 && !collision){
+            animator.SetBool("Up", true);
+            animator.SetBool("Down", false);
+        }
+
+        if (rb.velocity.y < 0.0 && !collision){
+            animator.SetBool("Down", true);
+            animator.SetBool("Up", false);
+        }
+
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
     }
-  
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        
+        if (collision.collider.tag == "Ground"){
+            this.collision = true;
+            Debug.Log("Ground");
+            animator.SetBool("Down", false);
+            animator.SetBool("Up", false);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision){
+        if (collision.collider.tag == "Ground"){
+            this.collision = false;
+        }
+
+    }
+
 
 }
